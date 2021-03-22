585d5c8babd0215de4d80ba14d727f6679502e68
dawidePl <47758566+dawidePl@users.noreply.github.com>
2021-03-22 23:12:55 +0100

    Fixed typo in embed :)

--- src/commands/weather/weather.js
+++ src/commands/weather/weather.js
@@ -26,7 +26,7 @@ module.exports = {
                                     .addFields(
                                         { name: '🌡️ Temperature data     ', value: `${data.current.temp_c}°C\n${data.current.temp_f}°F`, inline: true },
                                         { name: '🌥️ Atmospheric data', value: `Humidity: ${data.current.humidity}%\nPressure: ${data.current.pressure_mb} hPa\nVisibility: ${data.current.vis_km} kilometers ( ${data.current.vis_miles} miles )`, inline: true },
-                                        { name: '🕛 Time date', value: `Local date: ${data.location.localtime.split(' ')[0].split('-').join('.')}\nLocal time: ${data.location.localtime.split(' ')[1]} ( 24 hour clock )`, inline: false }
+                                        { name: '🕛 Time data', value: `Local date: ${data.location.localtime.split(' ')[0].split('-').join('.')}\nLocal time: ${data.location.localtime.split(' ')[1]} ( 24 hour clock )`, inline: false }
                                     )
                                     .setThumbnail(`https:${data.current.condition.icon}`)
                                     .setColor('2F3136')
