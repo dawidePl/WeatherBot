131712a7ce9dca891a73c6ed3acfbcc2067ccf29
dawidePl <47758566+dawidePl@users.noreply.github.com>
2021-03-23 11:48:17 +0100

    Added wind data

--- src/commands/weather/weather.js
+++ src/commands/weather/weather.js
@@ -21,12 +21,32 @@ module.exports = {
             return response.json();
         })
         .then(data => {
+            wind_direction = {
+                W: 'West',
+                E: 'East',
+                N: 'North',
+                S: 'South',
+                NE: 'Northeast',
+                SE: 'Southeast',
+                SW: 'Southwest',
+                NW: 'NorthWest',
+                NNE: 'North-Northeast',
+                ENE: 'East-Northease',
+                ESE: 'East-Southeast',
+                SSE: 'South-Southeast',
+                SSW: 'South-Southwest',
+                WSW: 'West-Soutwest',
+                WNW: 'North-Northwest',
+                NNW: 'North-Northwest',
+            }
+
             const embed = new Discord.MessageEmbed()
                                     .setTitle(`${data.location.name}, ${data.location.country}`)
                                     .addFields(
                                         { name: '🌡️ Temperature data     ', value: `${data.current.temp_c}°C\n${data.current.temp_f}°F`, inline: true },
                                         { name: '🌥️ Atmospheric data', value: `Humidity: ${data.current.humidity}%\nPressure: ${data.current.pressure_mb} hPa\nVisibility: ${data.current.vis_km} kilometers ( ${data.current.vis_miles} miles )`, inline: true },
-                                        { name: '🕛 Time data', value: `Local date: ${data.location.localtime.split(' ')[0].split('-').join('.')}\nLocal time: ${data.location.localtime.split(' ')[1]} ( 24 hour clock )`, inline: false }
+                                        { name: '🕛 Time data', value: `Local date: ${data.location.localtime.split(' ')[0].split('-').join('.')}\nLocal time: ${data.location.localtime.split(' ')[1]} ( 24 hour clock )` },
+                                        { name: '💨 Wind data', value: `Wind speed: ${data.current.wind_kph} km/h ( ${data.current.wind_mph} mph )\nWind direction: ${wind_direction[data.current.wind_dir] || data.current.wind_dir}`, inline: true }
                                     )
                                     .setThumbnail(`https:${data.current.condition.icon}`)
                                     .setColor('2F3136')
